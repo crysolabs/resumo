7d73bc1c70e48a2a9963fd081c4dcc571d893d54
Dark Waves Tech <89140466+darkwaves-ofc@users.noreply.github.com>
2025-03-29 19:19:44 +0530

    Update auth.ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -72,7 +72,7 @@ export const authOptions: NextAuthOptions = {
     async jwt({ token, user }) {
       const dbUser = await prisma.user.findFirst({
         where: {
-          email: token.email,
+          email: token.email as string,
         },
       })
 
